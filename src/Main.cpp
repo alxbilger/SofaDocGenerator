265fe24178e6bcfcc04341a65596b9f9323211c4
Alex Bilger <alexbilger0@gmail.com>
2024-05-06 11:11:07 +0200

    skip MORUnilateralInteractionConstraint

--- src/Main.cpp
+++ src/Main.cpp
@@ -231,6 +231,7 @@ void generateDoc(std::string outputDirectory)
         if (entry->className != "Distances"
             && entry->className != "ContactListener"
             && entry->className != "FileMessageHandlerComponent"
+            && entry->className != "MORUnilateralInteractionConstraint"
             ) //skip because these component is buggy
         {
             std::cout << entry->className << std::endl;
